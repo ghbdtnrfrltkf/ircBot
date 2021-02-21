240fb6445c9ee4cde05b9bfbba59aeeb79ded18f
ghbdtnvbh <34269807+ghbdtnvbh@users.noreply.github.com>
2021-02-21 20:03:11 +0300

    Update ircBot.go

--- ircBot.go
+++ ircBot.go
@@ -1,73 +1,78 @@
 package main
 
-import ("net"
-        "log"
-        "net/textproto"
-        "bufio"
-        "fmt"
-        "strings"
-      )
-
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"net/textproto"
+	"strings"
+)
 
 type Bot struct {
-  nick      string
-  user      string
-  channel   string
-}  
+	nick    string
+	user    string
+	channel string
+}
 
 type Server struct {
-    addr  string
-    port    string
+	addr string
+	port string
 }
 
-var NewBot = Bot {
-  nick:     "simple_bot",
-  channel:  "#ch",
-  user:     "bot",
+var NewBot = Bot{
+	nick:    "simple_bot",
+	channel: "#channel",
+	user:    "simple_bot",
 }
 
-var NewServer = Server {
-    addr:   "127.0.0.1",
-    port:  "6667",
+var NewServer = Server{
+	addr: "127.0.0.1",
+	port: "6667",
 }
 
 func (serv *Server) Connect() (conn net.Conn, err error) {
-  conn, err = net.Dial("tcp", serv.addr + ":" + serv.port)
-  if err != nil {
-    log.Fatal("unable to connect to IRC server ", err)
-  }
+	conn, err = net.Dial("tcp", serv.addr+":"+serv.port)
+	if err != nil {
+		log.Fatal("unable to connect to IRC server ", err)
+	}
 
-  log.Printf("Connected to IRC server %s (%s)\n", serv.addr, conn.RemoteAddr())
-  return conn, nil
+	log.Printf("Connected to IRC server %s (%s)\n", serv.addr, conn.RemoteAddr())
+	return conn, nil
 }
 
-func main(){
-  serv := NewServer
-  ircBot := NewBot
-  conn, _ := serv.Connect()
-  conn.Write([]byte("USER " + ircBot.user +" 8 * :" + ircBot.user + "\r\n"))
-  conn.Write([]byte("NICK " + ircBot.nick + "\r\n"))
-  conn.Write([]byte("JOIN " + ircBot.channel + "\r\n"))
-  defer conn.Close()
+func main() {
+	serv := NewServer
+	ircBot := NewBot
+	conn, _ := serv.Connect()
+	conn.Write([]byte("USER " + ircBot.user + " 8 * :" + ircBot.user + "\r\n"))
+	conn.Write([]byte("NICK " + ircBot.nick + "\r\n"))
+	conn.Write([]byte("JOIN " + ircBot.channel + "\r\n"))
 
-  reader := bufio.NewReader(conn)
-  tp := textproto.NewReader(reader)
-  for {
-        line, err := tp.ReadLine()
-        if err != nil {
-            break // break loop on errors
-        }
-        if strings.HasPrefix(line, "PING") {
-			conn.Write([]byte("PONG\n"))
-        }
-		if strings.Contains(line, "JOIN #ch") {
+	reader := bufio.NewReader(conn)
+	tp := textproto.NewReader(reader)
+	defer conn.Close()
 
-		   s := strings.Trim(line, ":JOIN #ch")
-		   ss :=  strings.Split(s, "!")[0]
-		   fmt.Fprintf(conn, "PRIVMSG " + ircBot.channel + " :Добро Пожаловать " + ss + " \r\n")
+	for {
+		line, err := tp.ReadLine()
+		if err != nil {
+			break // break loop on errors
+		}
+		if strings.HasPrefix(line, "PING :") {
+			PongKey := strings.Trim(line, "PING :")
+			conn.Write([]byte("PONG :" + PongKey))
+		}
+		if strings.Contains(line, "REGISTER") {
+			conn.Write([]byte("JOIN " + ircBot.channel + "\r\n"))
+		}
+		if strings.Contains(line, ":") {
+
+			st := strings.Trim(line, "JOIN :#channel")
+			ss := strings.Split(st, "!")[0]
+			fmt.Fprintf(conn, "PRIVMSG "+ircBot.channel+" :Добро Пожаловать "+ss+" \r\n")
 		}
 
-        fmt.Printf("%s\n", line)
-    }
+		fmt.Printf("%s\n", line)
+	}
 
 }
