7b2ccafa72e60dbf10a9668ef0b884fda042fd9e
ghbdtnvbh <an0nym0us7@protonmail.ch>
2021-03-21 16:46:10 +0300

    add goroutine

--- ircBot.go
+++ ircBot.go
@@ -7,47 +7,44 @@ import (
 	"net"
 	"net/textproto"
 	"strings"
+	"time"
 )
 
 type Bot struct {
 	nick    string
 	user    string
 	channel string
-}
-
-type Server struct {
-	addr string
-	port string
+	addr    string
+	port    string
 }
 
 var NewBot = Bot{
 	nick:    "WelcomeBot",
 	channel: "#ru",
 	user:    "WelcomeBot",
+	addr:    "127.0.0.1",
+	port:    "6667",
 }
 
-var NewServer = Server{
-	addr: "127.0.0.1",
-	port: "6667",
-}
-
-func (serv *Server) Connect() (conn net.Conn, err error) {
-	conn, err = net.Dial("tcp", serv.addr+":"+serv.port)
+func (bot *Bot) Connect() (conn net.Conn, err error) {
+	conn, err = net.Dial("tcp", bot.addr+":"+bot.port)
 	if err != nil {
 		log.Fatal("unable to connect to IRC server ", err)
 	}
 
-	log.Printf("Connected to IRC server %s (%s)\n", serv.addr, conn.RemoteAddr())
+	log.Printf("Connected to IRC server %s (%s)\n", bot.addr, conn.RemoteAddr())
 	return conn, nil
 }
 
 func main() {
-	serv := NewServer
 	ircBot := NewBot
-	conn, _ := serv.Connect()
+	conn, _ := ircBot.Connect()
 	conn.Write([]byte("USER " + ircBot.user + " 8 * :" + ircBot.user + "\r\n"))
 	conn.Write([]byte("NICK " + ircBot.nick + "\r\n"))
-	conn.Write([]byte("JOIN " + ircBot.channel + "\r\n"))
+	go func() {
+		time.Sleep(15 * time.Second)
+		conn.Write([]byte("JOIN " + ircBot.channel + "\r\n"))
+	}()
 
 	reader := bufio.NewReader(conn)
 	tp := textproto.NewReader(reader)
@@ -62,10 +59,10 @@ func main() {
 			pongKey := strings.Trim(line, "PING :")
 			conn.Write([]byte("PONG :" + pongKey + "\r\n"))
 		}
-		if strings.Contains(line, "REGISTER") {
-			conn.Write([]byte("JOIN " + ircBot.channel + "\r\n"))
-		}
-		if strings.Contains(line, "JOIN #ru") {
+		// if strings.Contains(line, "REGISTER") {
+		// conn.Write([]byte("JOIN " + ircBot.channel + "\r\n"))
+		// }
+		if strings.Contains(line, "JOIN :#ru") {
 			st := strings.Trim(line, ":")
 			ss := strings.Split(st, "!")[0]
 			if ss != "WelcomeBot" {
